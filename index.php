29e5f79dd1e02e2017448a0675aef620a0c279f1
zakali23 <zakaria.zekraoui@gmail.com>
2018-03-08 22:09:27 +0100

    ajouter des font

--- index.php
+++ index.php
@@ -153,11 +153,12 @@
             </section>
 
           </header>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
           <script>
           window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')
           </script>
-          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
           <script src="js/midnight.jquery.min.js"></script>
           <script>
           $('.scrollTo').click(function() {
@@ -176,7 +177,7 @@ return false;
           // Start midnight
           $(document).ready(function(){
             // Change this to the correct selector.
-            $('').midnight();
+            $('nav').midnight();
           });
           </script>
 
