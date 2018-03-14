68c779c039d465d98e05bfa72ac348628ccd3490
Jennifer Lieby <ze_nifer@hotmail.com>
2018-03-14 16:07:29 +0100

    jout des liens dans le navigateur, sur actualite et activité + et je sais plus

--- activites.php
+++ activites.php
@@ -10,7 +10,7 @@
   <title>ELSAS'Sport</title>
 
   <!-- Bootstrap core CSS -->
-  
+
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 
@@ -21,8 +21,38 @@
 <body>
 
 <!--     __________HEADER____________        -->
-<?php include"header-2.php";
-?>
+<!-- Barre de Navigation -->
+
+        <nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <div><a  href="#">
+        <img src="../img/logo-blanc.png" alt="logo" height="50" width="auto" />
+      </a>
+      </div>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.php">Accueil </a></li>
+        <li class="active"><a href="activite.php">Activités <span class="sr-only">(current)</span></a></li>
+        <li><a href="actualites.php">Actualités</a></li>
+        <li><a href="contact.php" >Contact </a>
+        </li>
+
+      </ul>
+
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 
 
 
--- actualites.php
+++ actualites.php
@@ -24,8 +24,38 @@
 <body>
 
   <!--     __________HEADER____________        -->
-  <?php include"header-2.php";
-  ?>
+  <!-- Barre de Navigation -->
+
+          <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <div><a  href="#">
+          <img src="../img/logo-blanc.png" alt="logo" height="50" width="auto" />
+        </a>
+        </div>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="index.php">Accueil </a></li>
+          <li><a href="activites.php">Activités </a></li>
+          <li class="active"><a href="actualites.php">Actualités <span class="sr-only">(current)</span></a></li>
+          <li><a href="contact.php" >Contact </a>
+          </li>
+
+        </ul>
+
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
 
 <!-- carousel -->
   <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
