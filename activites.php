dc5ae0a820d2381c75e4a9db27d342b96dc32d27
zakali23 <zakaria.zekraoui@gmail.com>
2018-03-15 14:26:55 +0100

    modification footer

--- activites.php
+++ activites.php
@@ -1,256 +1,239 @@
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="img/logo.svg">
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="img/logo.svg">
 
-    <title>ELSAS'Sport</title>
+  <title>ELSAS'Sport</title>
 
-    <!-- Bootstrap core CSS -->
+  <!-- Bootstrap core CSS -->
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 
-    <!-- Custom styles for this template -->
-    <link href="css/header-jennifer.css" rel="stylesheet">
-      <link href="css/style-footer.css" rel="stylesheet">
-      <link href="css/style-jennifer.css" rel="stylesheet">
-  </head>
+  <!-- Custom styles for this template -->
+  <link href="css/header-jennifer.css" rel="stylesheet">
+    <link href="css/style-footer.css" rel="stylesheet">
+    <link href="css/style-jennifer.css" rel="stylesheet">
+</head>
 
-  <body>
+<body>
 
-  <!--     __________HEADER____________        -->
-  <!-- Barre de Navigation -->
+<!--     __________HEADER____________        -->
+<!-- Barre de Navigation -->
 
-          <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <div><a  href="#">
-          <img src="img/logo-blanc.png" alt="logo" height="50" width="auto" />
-        </a>
-        </div>
+        <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <div><a  href="#">
+        <img src="img/logo-blanc.png" alt="logo" height="50" width="auto" />
+      </a>
       </div>
-
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="index.php">Accueil </a></li>
-          <li class="active"><a href="activite.php">Activités <span class="sr-only">(current)</span></a></li>
-          <li><a href="actualites.php">Actualités</a></li>
-          <li><a href="contact.php" >Contact </a>
-          </li>
-
-        </ul>
-
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
-
-
-
-  <!--     __________CAROUSEL____________        -->
-
-  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <img src="img/trail1.jpg" alt="...">
-        <div class="carousel-caption">
-          <h1>Vous êtes mordus de Trail ?</h1>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/rugby-club.jpg" alt="...">
-        <div class="carousel-caption">
-          <h1>Plutôt fan de Rugby ?</h1>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/fitness4.jpg" alt="...">
-        <div class="carousel-caption">
-          <h1>Ou vous préfèrez soulever de la fonte ?</h1>
-        </div>
-      </div>
-
     </div>
 
-    <!-- Controls -->
-    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.php">Accueil </a></li>
+        <li class="active"><a href="activite.php">Activités <span class="sr-only">(current)</span></a></li>
+        <li><a href="actualites.php">Actualités</a></li>
+        <li><a href="contact.php" >Contact </a>
+        </li>
 
-  <!-- __________________ -->
+      </ul>
 
-  <!-- espace -->
-  <div class="espace">
-  </div>
-
-  <!-- Titre -->
-  <h1 class="title">Découvrez toutes nos Activités !</h1>
-
-  <!-- espace -->
-  <div class="espace">
-  </div>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 
 
-  <!-- Texte -->
 
-  <div class="texte">
-      <p>Vous êtes fanatique de vélo ? Ou bien votre truc c'est plutôt le soulevé de terre ? Ou alors vous êtes plutôt zen et fan de Yoga ou Pilates ?
-      <br />
-      Quelle que soit votre activité préférée, à Elsas'Sport vous pourrez pratiquer le sport que vous souhaitez avec d'autres passionnés.
-      <br />
-      Grâce à une communauté active, une région dynamique et attractive et des membres enthousiastes voire exaltés, plus d'excuse pour ne pas vous (re)mettre au sport !
-      <br />
-      <br />
-      Sports collectifs, individuels, en salle, en pleine nature, … : chez Elsas'Sport les différentes pratiques sont toutes représentées.
-      <br />
-      Course à pied, Football, Yoga, Randonnée, Fitness, Vélo, Escalade, … vous trouverez forcément le sport qui vous plait et quelqu'un avec qui pratiquer, et si jamais ce n'est pas le cas, rien ne vous empêche d'organiser vous même un évènement ! ;)
-      </p>
-  </div>
+<!--     __________CAROUSEL____________        -->
 
-  </br></br>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
 
-  <!-- ____________IMAGES__________ -->
-
-  <div class="col-xs-12 col-sm-11 col-sm-offset-1" >
-    <!-- ________Colonne 1 -->
-    <div class="row"><!-- __row -->
-            <div class="col-sm-5"><!-- __col -->
-                <div class="thumbnail texte-thumbnails type_sport"><!-- __thumb -->
-                    <img src="img/sport-collectif-2.jpg" alt="sport-en-salle">
-                    <div class="caption"><!-- __caption -->
-                        <h3 class="sous_titre">Sports Collectifs</h3>
-                        <br/>
-                        <p>Football, Basket, Badminton, … : autant de sports où il faut trouver d'autres motivés !
-                          <br/>
-                          Comme il est souvent difficile de réunir une équipe, à Elsas'Sport il est possible de trouver des nombreux évènements à rejoindre ou d'en proposer un !
-                        </p>
-                    </div><!-- __caption -->
-                </div><!-- __thumb -->
-            </div><!-- __col -->
-
-            <div class="col-sm-5 col-sm-offset-1">
-                <div class="thumbnail texte-thumbnails type_sport">
-                    <img src="img/sport-nature.jpg" alt="chaussette-harry-potter">
-                    <div class="caption">
-                        <h3 class="sous_titre">Sport Nature</h3>
-                        <br/>
-                        <p>Randonnée, Trail, Escalade en falaise, … : autant de sports qui se pratiquent en plein air !
-                        <br/>
-                        En Alsace on a la chance d'avoir des terrains variés : plaine rhénane, Piémont des Vosges, début des Vosges, … : vous trouverez forcément une sortie qui vous botte !</p>
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
-<!-- essai1 -->
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="img/trail1.jpg" alt="...">
+      <div class="carousel-caption">
+        <h1>Vous êtes mordus de Trail ?</h1>
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/rugby-club.jpg" alt="...">
+      <div class="carousel-caption">
+        <h1>Plutôt fan de Rugby ?</h1>
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/fitness4.jpg" alt="...">
+      <div class="carousel-caption">
+        <h1>Ou vous préfèrez soulever de la fonte ?</h1>
+      </div>
+    </div>
 
   </div>
 
-  <!-- essai2 -->
-  <div class="col-xs-12 col-sm-11 col-sm-offset-1" >
-    <!-- ________Colonne 1 -->
-    <div class="row"><!-- __row -->
-            <div class="col-sm-5"><!-- __col -->
-              <div class="thumbnail texte-thumbnails type_sport">
-                  <img src="img/sport-salle.jpg" alt="sport-en-salle">
-                  <div class="caption">
-                      <h3 class="sous_titre">Sport en salle</h3>
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<!-- __________________ -->
+
+<!-- espace -->
+<div class="espace">
+</div>
+
+<!-- Titre -->
+<h1 class="title">Découvrez toutes nos Activités !</h1>
+
+<!-- espace -->
+<div class="espace">
+</div>
+
+
+<!-- Texte -->
+
+<div class="texte">
+    <p>Vous êtes fanatique de vélo ? Ou bien votre truc c'est plutôt le soulevé de terre ? Ou alors vous êtes plutôt zen et fan de Yoga ou Pilates ?
+    <br />
+    Quelle que soit votre activité préférée, à Elsas'Sport vous pourrez pratiquer le sport que vous souhaitez avec d'autres passionnés.
+    <br />
+    Grâce à une communauté active, une région dynamique et attractive et des membres enthousiastes voire exaltés, plus d'excuse pour ne pas vous (re)mettre au sport !
+    <br />
+    <br />
+    Sports collectifs, individuels, en salle, en pleine nature, … : chez Elsas'Sport les différentes pratiques sont toutes représentées.
+    <br />
+    Course à pied, Football, Yoga, Randonnée, Fitness, Vélo, Escalade, … vous trouverez forcément le sport qui vous plait et quelqu'un avec qui pratiquer, et si jamais ce n'est pas le cas, rien ne vous empêche d'organiser vous même un évènement ! ;)
+    </p>
+</div>
+
+</br></br>
+
+<!-- ____________IMAGES__________ -->
+
+<div class="col-xs-12 col-sm-11 col-sm-offset-1" >
+  <!-- ________Colonne 1 -->
+  <div class="row"><!-- __row -->
+          <div class="col-sm-5"><!-- __col -->
+              <div class="thumbnail texte-thumbnails type_sport"><!-- __thumb -->
+                  <img src="img/sport-collectif-2.jpg" alt="sport-en-salle">
+                  <div class="caption"><!-- __caption -->
+                      <h3 class="sous_titre">Sports Collectifs</h3>
                       <br/>
-                      <p>Vous hésitez encore à franchir la porte du club de gym près de chez vous. Ce type de club est-il vraiment fait pour vous?
-                      <br/>
-                      Pour les personnes qui ont besoin de se motiver régulièrement, la salle de sport offre une bonne solution dans la mesure où de très nombreuses activités sont proposées !
+                      <p>Football, Basket, Badminton, … : autant de sports où il faut trouver d'autres motivés !
+                        <br/>
+                        Comme il est souvent difficile de réunir une équipe, à Elsas'Sport il est possible de trouver des nombreux évènements à rejoindre ou d'en proposer un !
                       </p>
+                  </div><!-- __caption -->
+              </div><!-- __thumb -->
+          </div><!-- __col -->
+
+          <div class="col-sm-5 col-sm-offset-1">
+              <div class="thumbnail texte-thumbnails type_sport">
+                  <img src="img/sport-nature.jpg" alt="chaussette-harry-potter">
+                  <div class="caption">
+                      <h3 class="sous_titre">Sport Nature</h3>
+                      <br/>
+                      <p>Randonnée, Trail, Escalade en falaise, … : autant de sports qui se pratiquent en plein air !
+                      <br/>
+                      En Alsace on a la chance d'avoir des terrains variés : plaine rhénane, Piémont des Vosges, début des Vosges, … : vous trouverez forcément une sortie qui vous botte !</p>
                   </div>
               </div>
-            </div><!-- __col -->
-
-            <div class="col-sm-5 col-sm-offset-1">
-                <div class="thumbnail texte-thumbnails type_sport">
-                  <img src="img/sport-individuel-2.jpg" alt="chaussette-harry-potter">
-                  <div class="caption">
-                      <h3 class="sous_titre">Sports Individuels</h3>
-                      <br/>
-                      <p>Course à pied, Vélo, Natation, … : à priori pas besoin de compagnons pour faire tout ça mais avouez que c'est quand même plus sympa non ?
-                      <br/>
-                      À Elsas'sport vous trouverez forcément d'autres motivés pour pédaler, nager, courir, ou tout simplement transpirer !
-                      </p>
-                  </div>
-                </div>
-            </div>
+          </div>
 
 
-        </div>
+      </div>
 <!-- essai1 -->
 
-  </div>
+</div>
+
+<!-- essai2 -->
+<div class="col-xs-12 col-sm-11 col-sm-offset-1" >
+  <!-- ________Colonne 1 -->
+  <div class="row"><!-- __row -->
+          <div class="col-sm-5"><!-- __col -->
+            <div class="thumbnail texte-thumbnails type_sport">
+                <img src="img/sport-salle.jpg" alt="sport-en-salle">
+                <div class="caption">
+                    <h3 class="sous_titre">Sport en salle</h3>
+                    <br/>
+                    <p>Vous hésitez encore à franchir la porte du club de gym près de chez vous. Ce type de club est-il vraiment fait pour vous?
+                    <br/>
+                    Pour les personnes qui ont besoin de se motiver régulièrement, la salle de sport offre une bonne solution dans la mesure où de très nombreuses activités sont proposées !
+                    </p>
+                </div>
+            </div>
+          </div><!-- __col -->
+
+          <div class="col-sm-5 col-sm-offset-1">
+              <div class="thumbnail texte-thumbnails type_sport">
+                <img src="img/sport-individuel-2.jpg" alt="chaussette-harry-potter">
+                <div class="caption">
+                    <h3 class="sous_titre">Sports Individuels</h3>
+                    <br/>
+                    <p>Course à pied, Vélo, Natation, … : à priori pas besoin de compagnons pour faire tout ça mais avouez que c'est quand même plus sympa non ?
+                    <br/>
+                    À Elsas'sport vous trouverez forcément d'autres motivés pour pédaler, nager, courir, ou tout simplement transpirer !
+                    </p>
+                </div>
+              </div>
+          </div>
+
+
+      </div>
+<!-- essai1 -->
+
+</div>
 
 <br /><br />
 
 
 
 
-  <!-- ________Colonne 2 -->
-  <div class="row">
+<!-- ________Colonne 2 -->
+<div class="row">
 
-  </div><!-- ________Colonne 2 -->
-  </br></br>
-
-
-
-<<<<<<< HEAD
-              <div class="col-sm-5 col-sm-offset-1 ">
-                  <div class="thumbnail texte-thumbnails type_sport">
-                      <img src="img/sport-individuel-2.jpg" alt="chaussette-harry-potter">
-                      <div class="caption">
-                          <h3 class="sous_titre">Sports Individuels</h3>
-                          <br/>
-                          <p>Course à pied, Vélo, Natation, … : à priori pas besoin de compagnons pour faire tout ça mais avouez que c'est quand même plus sympa non ?
-                          <br/>
-                          À Elsas'sport vous trouverez forcément d'autres motivés pour pédaler, nager, courir, ou tout simplement transpirer !
-                          </p>
-                      </div>
-                  </div>
-              </div>
-=======
-
->>>>>>> 9ece11c79c678eb77fb21ec011e81dc2bd37b9c9
-
-          </div><!-- ________1er div -->
-
-  <?php
-  include 'footer.php'; ?>
+</div><!-- ________Colonne 2 -->
+</br></br>
 
 
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-  <script src="js/holder.min.js"></script>
-    </body>
-  </html>
+
+
+
+
+<?php
+include 'footer.php'; ?>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+<script src="js/holder.min.js"></script>
+  </body>
+</html>
